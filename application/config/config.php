be607befac92d02a3d8324a52f568b42a600a2a3
Amit Kamble <amit.kamble015@gmail.com>
2025-07-10 17:16:22 +0530

    new version added php 8.2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ date_default_timezone_set('Asia/Kolkata');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/AMIT/proprtydb/';
+$config['base_url'] = 'http://localhost/AMITK/proprtydb/';
 // $config['base_url'] = 'https://www.visionbankingservices.com/';
 
 /*
